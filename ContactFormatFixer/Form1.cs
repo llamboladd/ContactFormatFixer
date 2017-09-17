ba95a0f1fffed4c98b4d4af35a1ede129d31c276
llamboladd <llamboladd@gmail.com>
2017-09-16 22:26:10 -0500

    Class Types for IO created.

--- ContactFormatFixer/Form1.cs
+++ ContactFormatFixer/Form1.cs
@@ -21,7 +21,33 @@ namespace ContactFormatFixer
         public class inputLine
         {
             //  The format of the input is as such:
-            //  First Name, Last Name, 
+            //  First Name, Last Name, Family Name, Email Address, Phone Number
+
+            public string firstName     { get; set; }
+            public string lastName      { get; set; }
+            public string familyName    { get; set; }
+            public string emailAddress  { get; set; }
+            public string phoneNumber   { get; set; }
+
+            inputLine()
+            {
+                this.firstName = "";
+                this.lastName = "";
+                this.familyName = "";
+                this.emailAddress = "bugmenot@bugmenot.com";
+                this.phoneNumber = "";
+            }
+
+            inputLine(string firstName, string lastName, string familyName, string emailAddress, string phoneNumber)
+            {
+                this.firstName = firstName;
+                this.lastName = lastName;
+                this.familyName = familyName;
+                this.emailAddress = emailAddress;
+                this.phoneNumber = phoneNumber;
+            }
+
+
 
         }
 
@@ -30,7 +56,71 @@ namespace ContactFormatFixer
             //  The format of the output is as such:
             //  Name, Given Name, Additional Name, Family Name, Yomi Name, Given Name Yomi, Additional Name Yomi, Family Name Yomi, Name Prefix, Name Suffix, Initials, Nickname, Short Name, Maiden Name, Birthday, Gender, Location, Billing Information, Directory Server, Mileage, Occupation, Hobby, Sensitivity, Priority, Subject, Notes, Group Membership, E-mail 1 - Type, E-mail 1 - Value, E-mail 2 - Type, E-mail 2 - Value, IM 1 - Type, IM 1 - Service, IM 1 - Value, Phone 1 - Type, Phone 1 - Value, Phone 2 - Type, Phone 2 - Value, Phone 3 - Type, Phone 3 - Value, Address 1 - Type, Address 1 - Formatted, Address 1 - Street, Address 1 - City, Address 1 - PO Box, Address 1 - Region, Address 1 - Postal Code, Address 1 - Country, Address 1 - Extended Address, Organization 1 - Type, Organization 1 - Name, Organization 1 - Yomi Name, Organization 1 - Title, Organization 1 - Department, Organization 1 - Symbol, Organization 1 - Location, Organization 1 - Job Description, Website 1 - Type, Website 1 - Value
 
+            public string Name { get; set; }
+            public string GivenName { get; set; }
+            public string AdditionalName { get; set; }
+            public string FamilyName { get; set; }
+            public string YomiName { get; set; }
+            public string GivenNameYomi { get; set; }
+            public string AdditionalNameYomi { get; set; }
+            public string FamilyNameYomi { get; set; }
+            public string NamePrefix { get; set; }
+            public string NameSuffix { get; set; }
+            public string Initials { get; set; }
+            public string Nickname { get; set; }
+            public string ShortName { get; set; }
+            public string MaidenName { get; set; }
+            public string Birthday { get; set; }
+            public string Gender { get; set; }
+            public string Location { get; set; }
+            public string BillingInformation { get; set; }
+            public string DirectoryServer { get; set; }
+            public string Mileage { get; set; }
+            public string Occupation { get; set; }
+            public string Hobby { get; set; }
+            public string Sensitivity { get; set; }
+            public string Priority { get; set; }
+            public string Subject { get; set; }
+            public string Notes { get; set; }
+            public string GroupMembership { get; set; }
+            public string Email1Type { get; set; }
+            public string Email1Value { get; set; }
+            public string Email2Type { get; set; }
+            public string Email2Value { get; set; }
+            public string IM1Type { get; set; }
+            public string IM1Service { get; set; }
+            public string IM1Value { get; set; }
+            public string Phone1Type { get; set; }
+            public string Phone1Value { get; set; }
+            public string Phone2Type { get; set; }
+            public string Phone2Value { get; set; }
+            public string Phone3Type { get; set; }
+            public string Phone3Value { get; set; }
+            public string Address1Type { get; set; }
+            public string Address1Formatted { get; set; }
+            public string Address1Street { get; set; }
+            public string Address1City { get; set; }
+            public string Address1POBox { get; set; }
+            public string Address1Region { get; set; }
+            public string Address1PostalCode { get; set; }
+            public string Address1Country { get; set; }
+            public string Address1ExtendedAddress { get; set; }
+            public string Organization1Type { get; set; }
+            public string Organization1Name { get; set; }
+            public string Organization1YomiName { get; set; }
+            public string Organization1Title { get; set; }
+            public string Organization1Department { get; set; }
+            public string Organization1Symbol { get; set; }
+            public string Organization1Location { get; set; }
+            public string Organization1JobDescription { get; set; }
+            public string Website1Type { get; set; }
+            public string Website1Value { get; set; }
 
+            outputLine()
+            {
+                //  All values are initially set to null strings.
+                this.Name = this.GivenName = this.AdditionalName = this.FamilyName = this.YomiName = this.GivenNameYomi = this.AdditionalNameYomi = this.FamilyNameYomi = this.NamePrefix = this.NameSuffix = this.Initials = this.Nickname = this.ShortName = this.MaidenName = this.Birthday = this.Gender = this.Location = this.BillingInformation = this.DirectoryServer = this.Mileage = this.Occupation = this.Hobby = this.Sensitivity = this.Priority = this.Subject = this.Notes = this.GroupMembership = this.Email1Type = this.Email1Value = this.Email2Type = this.Email2Value = this.IM1Type = this.IM1Service = this.IM1Value = this.Phone1Type = this.Phone1Value = this.Phone2Type = this.Phone2Value = this.Phone3Type = this.Phone3Value = this.Address1Type = this.Address1Formatted = this.Address1Street = this.Address1City = this.Address1POBox = this.Address1Region = this.Address1PostalCode = this.Address1Country = this.Address1ExtendedAddress = this.Organization1Type = this.Organization1Name = this.Organization1YomiName = this.Organization1Title = this.Organization1Department = this.Organization1Symbol = this.Organization1Location = this.Organization1JobDescription = this.Website1Type = this.Website1Value = "";
+            }
         }
 
         public string originalFile = "";
