88fa43c63647343c026aa80290ba7fb0e4abedff
llamboladd <llamboladd@gmail.com>
2017-09-17 17:22:35 -0500

    More Functionality
    Yay

--- ContactFormatFixer/Form1.cs
+++ ContactFormatFixer/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace ContactFormatFixer
 {
@@ -20,16 +21,17 @@ namespace ContactFormatFixer
 
         public class inputLine
         {
+
             //  The format of the input is as such:
             //  First Name, Last Name, Family Name, Email Address, Phone Number
 
-            public string firstName     { get; set; }
-            public string lastName      { get; set; }
-            public string familyName    { get; set; }
-            public string emailAddress  { get; set; }
-            public string phoneNumber   { get; set; }
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public string familyName { get; set; }
+            public string emailAddress { get; set; }
+            public string phoneNumber { get; set; }
 
-            inputLine()
+            public inputLine()
             {
                 this.firstName = "";
                 this.lastName = "";
@@ -38,7 +40,7 @@ namespace ContactFormatFixer
                 this.phoneNumber = "";
             }
 
-            inputLine(string firstName, string lastName, string familyName, string emailAddress, string phoneNumber)
+            public inputLine(string firstName, string lastName, string familyName, string emailAddress, string phoneNumber)
             {
                 this.firstName = firstName;
                 this.lastName = lastName;
@@ -47,12 +49,54 @@ namespace ContactFormatFixer
                 this.phoneNumber = phoneNumber;
             }
 
+            public override string ToString()
+            {
+                return (this.firstName + "\t" + this.lastName + "\t" + this.familyName + "\t" + this.emailAddress + "\t" + this.phoneNumber);
+            }
 
+            public inputLine(outputLine outputLine)
+            {
+                this.firstName = outputLine.GivenName;
+                this.lastName = outputLine.FamilyName;
+                this.familyName = outputLine.FamilyName;
+                this.emailAddress = outputLine.Email1Value;
+                this.phoneNumber = outputLine.Phone1Value;
+            }
 
+            public outputLine toOutputLine()
+            {
+                outputLine newLine = new outputLine(this);
+                return newLine;
+            }
+
+            public List<string> toList()
+            {
+                List<string> newList = new List<string>();
+                newList.Add(this.firstName);
+                newList.Add(this.lastName);
+                newList.Add(this.familyName);
+                newList.Add(this.emailAddress);
+                newList.Add(this.phoneNumber);
+                return newList;
+            }
+
+            internal void fromList(List<string> lineItems)
+            {
+                this.firstName = lineItems.ElementAt(0);
+                this.lastName = lineItems.ElementAt(1);
+                this.familyName = lineItems.ElementAt(2);
+                this.emailAddress = lineItems.ElementAt(3);
+                this.phoneNumber = lineItems.ElementAt(4);
+            }
         }
+
+
+        
+
 
         public class outputLine
         {
+
             //  The format of the output is as such:
             //  Name, Given Name, Additional Name, Family Name, Yomi Name, Given Name Yomi, Additional Name Yomi, Family Name Yomi, Name Prefix, Name Suffix, Initials, Nickname, Short Name, Maiden Name, Birthday, Gender, Location, Billing Information, Directory Server, Mileage, Occupation, Hobby, Sensitivity, Priority, Subject, Notes, Group Membership, E-mail 1 - Type, E-mail 1 - Value, E-mail 2 - Type, E-mail 2 - Value, IM 1 - Type, IM 1 - Service, IM 1 - Value, Phone 1 - Type, Phone 1 - Value, Phone 2 - Type, Phone 2 - Value, Phone 3 - Type, Phone 3 - Value, Address 1 - Type, Address 1 - Formatted, Address 1 - Street, Address 1 - City, Address 1 - PO Box, Address 1 - Region, Address 1 - Postal Code, Address 1 - Country, Address 1 - Extended Address, Organization 1 - Type, Organization 1 - Name, Organization 1 - Yomi Name, Organization 1 - Title, Organization 1 - Department, Organization 1 - Symbol, Organization 1 - Location, Organization 1 - Job Description, Website 1 - Type, Website 1 - Value
 
@@ -116,17 +160,31 @@ namespace ContactFormatFixer
             public string Website1Type { get; set; }
             public string Website1Value { get; set; }
 
-            outputLine()
+            public outputLine()
             {
                 //  All values are initially set to null strings.
                 this.Name = this.GivenName = this.AdditionalName = this.FamilyName = this.YomiName = this.GivenNameYomi = this.AdditionalNameYomi = this.FamilyNameYomi = this.NamePrefix = this.NameSuffix = this.Initials = this.Nickname = this.ShortName = this.MaidenName = this.Birthday = this.Gender = this.Location = this.BillingInformation = this.DirectoryServer = this.Mileage = this.Occupation = this.Hobby = this.Sensitivity = this.Priority = this.Subject = this.Notes = this.GroupMembership = this.Email1Type = this.Email1Value = this.Email2Type = this.Email2Value = this.IM1Type = this.IM1Service = this.IM1Value = this.Phone1Type = this.Phone1Value = this.Phone2Type = this.Phone2Value = this.Phone3Type = this.Phone3Value = this.Address1Type = this.Address1Formatted = this.Address1Street = this.Address1City = this.Address1POBox = this.Address1Region = this.Address1PostalCode = this.Address1Country = this.Address1ExtendedAddress = this.Organization1Type = this.Organization1Name = this.Organization1YomiName = this.Organization1Title = this.Organization1Department = this.Organization1Symbol = this.Organization1Location = this.Organization1JobDescription = this.Website1Type = this.Website1Value = "";
+            }
+
+            public outputLine(inputLine inputLine)
+            {
+                this.Name = inputLine.firstName + " " + inputLine.lastName;
+                this.GivenName = inputLine.firstName;
+                this.FamilyName = inputLine.familyName;
+                this.Email1Value = inputLine.emailAddress;
+                this.Phone1Value = inputLine.phoneNumber;
+            }
+
+            public inputLine toInputLine()
+            {
+                inputLine newLine = new inputLine(this);
+                return newLine;
             }
         }
 
         public string originalFile = "";
         public string newFile = "";
-        public List<List<string>> originalList = new List<List<string>>();
-        public List<string> listItems = new List<string>();
+        public List<inputLine> listItems = new List<inputLine>();
 
         public void ShowMessage(String messageText)
         {
@@ -187,28 +245,18 @@ namespace ContactFormatFixer
             }
 
             List<string> originalLines = File.ReadAllLines(originalFile).ToList();
-
-            foreach (string fileLine in originalLines)
+            foreach (string originalLine in originalLines)
             {
-                List<string> lineItems = fileLine.Split(',').ToList();
-                foreach (string item in lineItems)
-                {
-                    listItems.Add(item);
-                }
-                originalList.Add(lineItems);
+                List<string> lineItems = originalLine.Split(',').ToList();
+                inputLine newLine = new inputLine();
+                newLine.fromList(lineItems);
+                listItems.Add(newLine);
             }
 
-            //foreach (string item in listItems)
-            //{
-            //    System.Console.Out.WriteLine(item.ToString());
-            //}
 
-            foreach (List<string> item in originalList)
+            foreach (inputLine lineItem in listItems)
             {
-                foreach (string stringItem in item)
-                {
-                    System.Console.Out.WriteLine(stringItem.ToString());
-                }
+                System.Console.Out.WriteLine(lineItem.ToString());
             }
 
             ShowMessage("File Fixed Successfully!");
